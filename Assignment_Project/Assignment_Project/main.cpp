8ec3a94e6f6bd961ae3d264bef9854401caf809f
kwYoohae <gocksdl7979@gmail.com>
2021-05-23 23:47:02 +0900

    template로 3개다 구현완료

--- Assignment_Project/Assignment_Project/main.cpp
+++ Assignment_Project/Assignment_Project/main.cpp
@@ -8,111 +8,45 @@
 using namespace std;
 
 void Print_Check(tree_node* pTemp);
-//void Open_Product(product_tree* Product);
+template <typename T>
+void Open_Category(T* type_Data,const char* text_file);
 //void Open_Location(location_tree* Location);
-void Open_Time(time_tree* Time);
-void Insert_data(time_tree* Time, tree_node* pParent, char* data);
+//void Open_Time(time_tree* Time);
+
+template <typename T>
+void Insert_data(T* type, tree_node* pParent, char* data);
 
 int main() {
 	product_tree Product;
 	location_tree Location;
 	time_tree Time;
-	Open_Time(&Time);
+	Open_Category(&Time,"time.txt");
+	Open_Category(&Product,"product.txt");
+	Open_Category(&Location,"location.txt");
 	Print_Check(Time.getRoot());
+	cout << '\n' << '\n';
+	Print_Check(Location.getRoot());
+	cout << '\n' << '\n';
+	Print_Check(Product.getRoot());
+	cout << '\n' << '\n';
 }
-
-void Open_Time(time_tree* Time) {
-	ifstream readFile("time.txt");
-	if (!readFile.is_open()) {
-		cout << "Error" << endl;
-		readFile.close();
-	}
-	tree_node* pParent = Time->getRoot();
-	char temp[100];
-	char data[100];
-	int len = 0;
-	int count = -1;
-	while (true) {
-		tree_node* pTemp = nullptr;
-		int i = 0;
-		memset(temp, NULL, 100);
-		memset(data, NULL, 100);
-		readFile.getline(temp, 100, '\n');
-		if (temp[0] == NULL)
-			break;
-		if (count == -1) {
-			while (temp[len] != '\0') {
-				data[i] = temp[len];
-				len++;
-				i++;
-				if (temp[len] == ' ') {
-					i = 0;
-					Insert_data(Time, Time->getRoot(), data);
-					memset(data, NULL, 100);
-					len++;
-				}
-				else if (temp[len] == '\0') {
-					Insert_data(Time, Time->getRoot(), data);
-					memset(data, NULL, 100);
-				}
-			}
-			pParent = Time->getRoot();
-			count++;
-			len = 0;
-		}
-		else {
-			while (temp[len] != ' ') {
-				data[len] = temp[len];
-				len++;
-			}
-			pTemp = pParent->getDown();
-			while (pTemp->getNext()) {
-				if (strcmp(pTemp->getData(), data) == 0)
-					break;
-				else
-					pTemp = pTemp->getNext();
-			}
-			if (pTemp) {
-				memset(data, NULL, 100);
-				len++;
-				while (temp[len] != '\0') {
-					data[i] = temp[len];
-					len++;
-					i++;
-					if (temp[len] == ' ') {
-						i = 0;
-						Insert_data(Time, pTemp, data);
-						memset(data, NULL, 100);
-						len++;
-					}
-					else if (temp[len] == '\0') {
-						Insert_data(Time, pTemp, data);
-						memset(data, NULL, 100);
-					}
-				}
-			}
-			len = 0;
-		}
-	}
-	readFile.close();
-}
-
 
 void Print_Check(tree_node* pTemp) {
 	if (pTemp) {
-		Print_Check(pTemp->getNext());
 		cout << pTemp->getData() << endl;
 		Print_Check(pTemp->getDown());
+		Print_Check(pTemp->getNext());
 	}
 }
 
-void Insert_data(time_tree* Time, tree_node* pParent, char* data) {
-	if (!Time->getRoot()) {
+template<typename T>
+void Insert_data(T* type, tree_node* pParent, char* data) {
+	if (!type->getRoot()) {
 		tree_node* pNew = new tree_node;
 		pNew->setData(data);
-		Time->setRoot(pNew);
+		type->setRoot(pNew);
 	}
-	else if (pParent == Time->getRoot()) {
+	else if (pParent == type->getRoot()) {
 		tree_node* pNew = new tree_node;
 		pNew->setData(data);
 		if(!pParent->getDown())
@@ -145,26 +79,81 @@ void Insert_data(time_tree* Time, tree_node* pParent, char* data) {
 		pNew->setUp(pParent);
 
 	}
-	
-	/*if (!Time->getRoot()) {
-		tree_node* pNew = new tree_node;
-		pNew->setData(data);
-		Time->setRoot(pNew);
+}
+
+template <typename T>
+void Open_Category(T* type_Data,const char *text_file) {
+	ifstream readFile(text_file);
+	if (!readFile.is_open()) {
+		cout << "Error" << endl;
+		readFile.close();
 	}
-	else if (pParent == Time->getRoot()) {
-		tree_node* pNew = new tree_node;
-		pNew->setNew();
-		pNew->setData(data);
-		pParent->setDown(pNew,pParent->getCount());
-		pNew->setUp(pParent);
-		pParent->setCount(pParent->getCount() + 1);
+	tree_node* pParent = type_Data->getRoot();
+	char temp[100];
+	char data[100];
+	int len = 0;
+	int count = -1;
+	while (true) {
+		tree_node* pTemp = nullptr;
+		int i = 0;
+		memset(temp, NULL, 100);
+		memset(data, NULL, 100);
+		readFile.getline(temp, 100, '\n');
+		if (temp[0] == NULL)
+			break;
+		if (count == -1) {
+			while (temp[len] != '\0') {
+				data[i] = temp[len];
+				len++;
+				i++;
+				if (temp[len] == ' ') {
+					i = 0;
+					Insert_data(type_Data, type_Data->getRoot(), data);
+					memset(data, NULL, 100);
+					len++;
+				}
+				else if (temp[len] == '\0') {
+					Insert_data(type_Data, type_Data->getRoot(), data);
+					memset(data, NULL, 100);
+				}
+			}
+			pParent = type_Data->getRoot();
+			count++;
+			len = 0;
+		}
+		else {
+			while (temp[len] != ' ') {
+				data[len] = temp[len];
+				len++;
+			}
+			pTemp = pParent->getDown();
+			while (pTemp->getNext()) {
+				if (strcmp(pTemp->getData(), data) == 0)
+					break;
+				else
+					pTemp = pTemp->getNext();
+			}
+			if (pTemp) {
+				memset(data, NULL, 100);
+				len++;
+				while (temp[len] != '\0') {
+					data[i] = temp[len];
+					len++;
+					i++;
+					if (temp[len] == ' ') {
+						i = 0;
+						Insert_data(type_Data, pTemp, data);
+						memset(data, NULL, 100);
+						len++;
+					}
+					else if (temp[len] == '\0') {
+						Insert_data(type_Data, pTemp, data);
+						memset(data, NULL, 100);
+					}
+				}
+			}
+			len = 0;
+		}
 	}
-	else {
-		tree_node* pNew = new tree_node;
-		pParent->setNew();
-		pNew->setData(data);
-		pParent->setDown(pNew, pParent->getCount());
-		pNew->setUp(pParent);
-		pParent->setCount(pParent->getCount() + 1);
-	}*/
+	readFile.close();
 }
